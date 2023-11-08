0f6fca240708eb6e62873501465ce42fec9e4bae
omnimrt <97639371+omnimrt@users.noreply.github.com>
2023-11-08 17:21:56 +0000

    Deploying to gh-pages from @ omnimrt/made-it-to-17-project@e5c53122db8c8ea36d1247d6f3bb9cef2ea03b10 🚀

--- main.js
+++
@@ -1,9 +0,0 @@
-let mobileMenuButton = document.querySelector('.header-mobile-menu');
-let navigationMenu = document.querySelector('.navigation-menu');
-let closeMenuButton = document.querySelector('.navigation-btn');
-mobileMenuButton.addEventListener('click', () => {
-  navigationMenu.classList.add('is-open');
-});
-closeMenuButton.addEventListener('click', () => {
-  navigationMenu.classList.remove('is-open');
-});
